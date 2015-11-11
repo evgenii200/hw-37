35ab16f644b1213f1d7af254a565ed25e6e1dd40
evgenii200 <avto-navigat@mail.ru>
2015-11-10 16:59:24 -0800

    Update TestRunner.sh

--- TestRunner.sh
+++ TestRunner.sh
@@ -34,5 +34,6 @@ cd ./$REPO_NAME
 
 mvn package
 echo "Executing Java programm ..."
-java -cp $HOME/$WS_DIR/$REPO_NAME/target/$REPO_NAME-$APP_VERSION.jar $MAIN_CLASS $ARGS_01
+#java -cp $HOME/$WS_DIR/$REPO_NAME/target/$REPO_NAME-$APP_VERSION.jar $MAIN_CLASS $ARGS_01
+ java -jar $HOME/$WS_DIR/$REPO_NAME/target/$REPO_NAME-$APP_VERSION-jar-with-dependencies.jar $MAIN_CLASS $ARGS_01
 
